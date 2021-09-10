4e44bb9ecfebe8d57c8a3623ab09bce3cd0188d7
Жмышенко Валерий Альбертович <75055942+datewithabullet@users.noreply.github.com>
2021-09-11 03:35:51 +0500

    Add files via upload

---
+++ BilliardsTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Billiards
+{
+    public static class BilliardsTask
+    {
+        /// <summary>
+        /// Расчитывает угол отскока шарика от стены в зависимости от наклона стены и направления движения
+        /// </summary>
+        /// <param name="directionRadians">Угол направления движения шара</param>
+        /// <param name="wallInclinationRadians">Угол</param>
+        /// <returns>Угол отскока шарика от стены</returns>
+        public static double BounceWall(double directionRadians, double wallInclinationRadians)
+        {
+            return 2 * wallInclinationRadians - directionRadians;
+        }
+    }
+}
